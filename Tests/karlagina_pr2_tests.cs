161faa3401169f89933d221f637a506de4bf24fc
lisi_ <lisi_@DESKTOP-OTMRBV2>
2026-03-22 23:03:17 +0500

    Добавьте файлы проекта.

---
+++ Tests/karlagina_pr2_tests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using karlagina_pr2;
+
+namespace karlagina_pr2_tests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void Test_Add()
+        {
+            dynamic result = Core.Exicute(8, '+', 3);
+            Assert.AreEqual(11, result);
+        }
+
+        [TestMethod]
+        public void Test_Subtract()
+        {
+            dynamic result = Core.Exicute(10, '-', 2);
+            Assert.AreEqual(8, result);
+        }
+
+        [TestMethod]
+        public void Test_Multiply()
+        {
+            dynamic result = Core.Exicute(7, '*', 9);
+            Assert.AreEqual(63, result);
+        }
+
+        [TestMethod]
+        public void Test_Divide()
+        {
+            dynamic result = Core.Exicute(25, '/', 5);
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void Test_Power()
+        {
+            dynamic result = Core.Exicute(2, '^', 2);
+            Assert.AreEqual(4, result);
+        }
+    }
+}
---
+++ karlagina_pr2/Core.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+
+namespace karlagina_pr2
+{
+    public class Core
+    {
+        public static dynamic Exicute(dynamic first, char op, dynamic second)
+        {
+            dynamic result = 0;
+            switch (op)
+            {
+                case '+':
+                    result = first + second;
+                    break;
+                case '-':
+                    result = first - second;
+                    break;
+                case '*':
+                    result = first * second;
+                    break;
+                case '/':
+                    result = first / second;
+                    break;
+                case '^':
+                    result = Math.Pow(first, second);
+                    break;
+                default:
+                    result = 0;
+                    break;
+            }
+            return result;
+        }
+    }
+}
